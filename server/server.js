0731614528325de942488ad4180c1ae44328c6f7
Israel Favour <fastarfavour@gmail.com>
2024-06-08 10:07:13 -0700

    Added connect and disconnect event handlers

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(fileName);
+const staticPath = path.join(__dirname, '../static');
+const port = process.env.PORT || 2000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) =>{
+    console.log('A client has just connected to server!');
+
+    socket.on('disconnect', ()=>{
+        console.log('client has disconnected!');
+    })
+
+})
+
+
+
+app.use(express.static(staticPath));
+
+server.listen(port, ()=>{
+    console.log(`Now listening on port ${port}`);
+});
