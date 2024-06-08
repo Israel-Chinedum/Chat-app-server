5c3454312b7b8145c2669ff1b17a1c8710d8bdee
Israel Favour <fastarfavour@gmail.com>
2024-06-08 16:19:35 -0700

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,17 +15,26 @@ const io = new Server(server);
 io.on('connection', (socket) =>{
     console.log('A client has just connected to server!');
 
+    socket.broadcast.emit('joined', `A new user just joined the chat at ${new Date().getTime()}`);
+
+    socket.emit('enter', {
+        from: 'Admin', 
+        text: 'Welcome to the chat room!'
+    });
+
     socket.on('disconnect', ()=>{
         console.log('client has disconnected!');
     });
 
     socket.on('createMessage', function(data){
         console.log('createMessage', data);
+        socket.emit('newMessage', `Hello ${data.from}, welcome again to the chat room!`);
 
-        io.emit('newMessage', {
+        socket.broadcast.emit('newMessage', {
             from: data.from,
-            text: data.text
-        });
+            text: data.text,
+            createdAt: new Date().getTime()
+        })
 
     });
 
