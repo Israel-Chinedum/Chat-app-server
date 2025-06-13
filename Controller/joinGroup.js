80e58f62f1750c2c3b4b9e56e2440e6107c876d2
Israel Favour <fastarfavour@gmail.com>
2025-06-13 11:18:52 -0700

    made more changes

---
+++ Controller/joinGroup.js
@@ -0,0 +1,1 @@
+export const joinGroup = (socket) => {};
--- Controller/socket.js
+++ Controller/socket.js
@@ -1,45 +1,46 @@
-import { messageRetriever } from './messageRetriever.js';
-import { privateMessaging } from './privateMessaging.js';
-import { createGroup } from './createGroup.js';
-import { userGroups } from './sendUserGroups.js';
-import { allGroups } from './sendGroups.js';
+import { messageRetriever } from "./messageRetriever.js";
+import { privateMessaging } from "./privateMessaging.js";
+import { createGroup } from "./createGroup.js";
+import { userGroups } from "./sendUserGroups.js";
+import { allGroups } from "./sendGroups.js";
+import { joinGroup } from "./joinGroup.js";
 
 const users = new Map();
 
 export const Socket = (io) => {
+  io.on("connection", (socket) => {
+    console.log("new user connected!");
 
-    io.on('connection', (socket) => {
+    const userId = socket.handshake.auth.userId;
+    console.log("user id:", socket.handshake);
+    userId && users.set(userId, socket.id);
+    console.log("users: ", users);
 
-        console.log('new user connected!');
-
-            const userId = socket.handshake.auth.userId
-            console.log('user id:', socket.handshake)
-            userId && users.set(userId, socket.id);
-            console.log('users: ', users);
-
-        socket.on('disconnect', () => {
-            console.log('user disconnected!');
-        })
-
-  
-        // HANDLE MESSAGE RELAYING
-        privateMessaging(socket, users, userId);
-
-        // RETRIEVE MESSAGES
-        messageRetriever(socket, userId);
-
-        // CREATE GROUPS
-        createGroup(socket, userId);
-
-        // SEND ALL GROUPS
-        socket.on('get-groups', () => {
-            allGroups(socket);
-        });
-
-        socket.on('get-myGroups', () => {
-            userGroups(socket, userId);
-        });
-    
+    socket.on("disconnect", () => {
+      console.log("user disconnected!");
     });
 
-}
+    // HANDLE MESSAGE RELAYING
+    privateMessaging(socket, users, userId);
+
+    // RETRIEVE MESSAGES
+    messageRetriever(socket, userId);
+
+    // CREATE GROUPS
+    createGroup(socket, userId);
+
+    //JOIN GROUP
+    socket.on("join-group", () => {
+      joinGroup(socket);
+    });
+
+    // SEND ALL GROUPS
+    socket.on("get-groups", () => {
+      allGroups(socket);
+    });
+
+    socket.on("get-myGroups", () => {
+      userGroups(socket, userId);
+    });
+  });
+};
